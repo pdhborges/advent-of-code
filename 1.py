ccfce7703acf01918b7e88038f6569565029dd6b
Pedro Borges <pdhborges@users.noreply.github.com>
2017-12-01 07:57:37 +0000

    Add solutions for day 1

---
+++ 1.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+from collections import deque
+
+def read(filename):
+    return Path(filename).read_text().rstrip()
+
+def circular_lshift(seq, amount):
+    shifted = deque(seq)
+    shifted.rotate(-amount)
+    return shifted
+    
+def score(seq, nexts_seq):
+    return sum(int(x) for x, next_x in zip(seq, nexts_seq) if x == next_x)
+
+seq = read('input-1-1.txt')
+nexts_seq = circular_lshift(seq, 1)
+
+print(score(seq, nexts_seq))
+
+
+seq = read('input-1-2.txt')
+nexts_seq = circular_lshift(seq, len(seq) // 2)
+
+print(score(seq, nexts_seq))
